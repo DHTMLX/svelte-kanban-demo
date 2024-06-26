4e83a21c8c7fe0cd6f30e11e1539665e328d10e3
Serhii Pylypchuk <pylypchukserhii1991@gmail.com>
2024-06-26 12:35:41 +0400

    [dev] add extended config

--- src/Kanban.svelte
+++ src/Kanban.svelte
@@ -1,34 +1,56 @@
 <script>
-    import { onMount, onDestroy } from "svelte";
-    import { Kanban, Toolbar } from "@dhx/trial-kanban";
-    import "@dhx/trial-kanban/dist/kanban.css";
-    
-    export let columns;
-    export let cards;
+  import { onMount, onDestroy } from "svelte";
+  import { Kanban, Toolbar, defaultEditorShape } from "@dhx/trial-kanban";
+  import "@dhx/trial-kanban/dist/kanban.css";
+  
+  export let columns;
+  export let cards;
+  export let rows;
+  export let cardShape;
 
-    let toolbar_container, kanban_container;
-    let kanban, toolbar;
+  let toolbar_container, kanban_container;
+  let kanban, toolbar;
 
-    onMount(() => {
-        kanban = new Kanban(kanban_container, {
-            columns, 
-            cards,
-            // other configuration properties
-        })
+  onMount(() => {
+    kanban = new Kanban(kanban_container, {
+      columns, 
+      cards,
+      rows,
+      rowKey: "type",
+      cardShape,
+      editorShape: [
+        ...defaultEditorShape, // import default config for editorShape
+        {
+          type: "links",
+          key: "links",
+          label: "Links"
+        },
+        {
+          type: "comments",
+          key: "comments",
+          label: "Comments",
+          config: {
+            placement: "editor"
+          }
+        }
+      ],
+      currentUser: 1,
+      // other configuration properties
+    })
 
-        toolbar = new Toolbar(toolbar_container, {
-            api: kanban.api,
-            // other configuration properties
-        })
-    });
+    toolbar = new Toolbar(toolbar_container, {
+      api: kanban.api,
+      // other configuration properties
+    })
+  });
 
-    onDestroy(() => {
-        kanban.destructor();
-        toolbar.destructor();
-    });
+  onDestroy(() => {
+    kanban.destructor();
+    toolbar.destructor();
+  });
 </script>
 
 <div class="component_container">
-    <div bind:this={toolbar_container}></div>
-    <div bind:this={kanban_container} style="height: calc(100% - 56px);"></div>
+  <div bind:this={toolbar_container}></div>
+  <div bind:this={kanban_container} style="height: calc(100% - 56px);"></div>
 </div>
